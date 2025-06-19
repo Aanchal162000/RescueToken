4381bebff800ff1fae9aaffef27a9c8e06b9942f
Aanchal162000 <aanchal@zkcross.network>
2025-06-19 19:20:24 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -88,7 +88,7 @@ export default function App() {
       setContractTokenBalance("0");
       setTokenContractAddress("");
     }
-  }, [walletAddress, contractAddressInput, ABI]);
+  }, [walletAddress, contractAddressInput]);
 
   const checkNetwork = useCallback(async () => {
     try {
